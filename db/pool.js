4923e412ee34dfa3d79858e81fc12249b9effdf8
Albert Jiang <galar@MacBook-Air.attlocal.net>
2024-08-20 21:57:12 -0500

    modify pool

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require('pg');
 
 module.exports = new Pool({
     // connectionString: `postgresql://${process.env.USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/anime_showdown`
-    connectionString:  DATABASE_PUBLIC_URL
+    connectionString:  process.env.DATABASE_PUBLIC_URL
 });
