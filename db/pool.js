631b87ae92d36576948c7b3e8e236bd9a0fb33ed
Albert Jiang <galar@MacBook-Air.attlocal.net>
2024-08-20 20:47:58 -0500

    remove files

--- db/pool.js
+++
@@ -1,5 +0,0 @@
-const { Pool } = require('pg');
-
-module.exports = new Pool({
-    connectionString: `postgresql://${process.env.USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/anime_showdown`
-});
